c28b21059cf1a09a501eef12e593bf785ac35721
Kotov Sergey <t9087620180@gmail.com>
2020-12-30 16:47:27 +0300

    add: first mark on map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles/main.scss';
+import Map from './js/components/map/Map';
+import { data } from './js/components/constants/constants';
+
+const map = new Map();
+
+const main = async () => {
+  try {
+    map.init(data);
+  } catch (err) {
+    // eslint-disable-next-line no-alert
+    alert(err);
+  }
+};
+
+main();
+console.log(data);
