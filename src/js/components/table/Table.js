befc52b1b096c7e86fdeecb401189a8bcce00117
Valentina Ponamareva <valiazin@gmail.com>
2021-01-05 21:17:29 +0300

    feat: add date and time

--- src/js/components/table/Table.js
+++ src/js/components/table/Table.js
@@ -11,7 +11,7 @@ export default class Table {
     const tableRow = document.createElement('tr');
     tableRow.classList.add('table__header');
 
-    const tableHeaders = ['№', 'Дата', 'Время', 'Тип проблемы', 'Проблема', 'Статус', 'Комментарий', 'Фото', 'Видео', 'Результат', 'Админ'];
+    const tableHeaders = ['№', 'Дата и время', 'Тип проблемы', 'Проблема', 'Статус', 'Комментарий', 'Фото', 'Видео', 'Результат', 'Админ'];
 
     tableHeaders.forEach((item) => {
       const tableHeader = document.createElement('th');
@@ -33,7 +33,6 @@ export default class Table {
       const cellsInRow = [
         this.createCell('chatId', chatId),
         this.createCell('date', date),
-        this.createCell('time', date),
         this.createCell('type', type),
         this.createCell('problem', problem),
         this.createCell('status', status),
@@ -68,15 +67,26 @@ export default class Table {
         break;
 
       case 'date':
-        tableCell.textContent = '01.01.2021';
-        break;
+        const problemDate = new Date(value);
+        const problemDay = problemDate.getDate();
+        const problemMonth = problemDate.getMonth();
+        const problemYear = problemDate.getFullYear();
+        const problemHour = problemDate.getHours();
+        const problemMin = problemDate.getMinutes();
 
-      case 'time':
-        tableCell.textContent = '20:30';
+        function addZero(n) {
+          return (parseInt(n, 10) < 10 ? '0' : '') + n;
+        }
+
+        tableCell.textContent = 
+        `${problemDay}.${problemMonth + 1}.${problemYear} ${addZero(problemHour)}:${addZero(problemMin)}`;
         break;
 
       case 'images':
-        
+        const image = document.createElement('img');
+        image.classList.add('table__row-photo');
+        image.setAttribute('src', ``);
+        tableCell.append(image);
         break;
 
       case 'movies':
