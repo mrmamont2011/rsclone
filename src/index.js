0432c1da832e6a4970219b2682cdd8938de29d71
kvadeck <kvadeck@yandex.ru>
2021-01-10 11:25:32 +0300

    feat: chage constant path, fix table name property

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles/main.scss';
 import Map from './js/components/map/Map';
 import Table from './js/components/table/Table';
-import { data } from './js/components/constants';
+import { data } from './constants';
 import renderFilter from './js/components/filter/filterRender';
 import getByFilter from './js/components/filter/filter';
 
